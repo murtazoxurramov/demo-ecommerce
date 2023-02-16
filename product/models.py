54a2ec1542e30601a8b51b2f20667f3b18bd9ac0
Developerboys <murtazoxurramov1@gmail.com>
2023-02-16 08:28:16 +0500

    fixed bugs

--- product/models.py
+++ product/models.py
@@ -5,7 +5,8 @@ from django.utils.text import slugify
 class ProductCategory(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(blank=True, null=True)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='child', null=True, blank=True)
+    parent = models.ForeignKey(
+        'self', on_delete=models.CASCADE, related_name='child', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -20,7 +21,7 @@ class ProductCategory(models.Model):
 
     def __str__(self):
         return self.title
-    
+
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -36,7 +37,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
-    
+
 
 class ProductImage(models.Model):
     image = models.ImageField(upload_to='uploads/product')
@@ -51,7 +52,7 @@ class ProductImage(models.Model):
 
     def __str__(self):
         return self.product.title
-    
+
 
 class ProductDiscount(models.Model):
     discount_percent = models.IntegerField()
@@ -67,19 +68,11 @@ class ProductDiscount(models.Model):
     def save(self, *args, **kwargs):
         discount = (int(self.product.prize) / 100) * self.discount_percent
         result = int(self.product.prize) - int(discount)
-        # self.product.prize = result
-        print(Product.objects.filter(id=self.product.title))
-        try:
-            changed_prize = Product.objects.filter(id=self.product.pk).get(
-                prize=result
-            )
-            changed_prize.save()
-        except:
-            print('Error')
+        changed_prize = Product.objects.filter(
+            id=self.product.pk).get(id=self.product.pk)
+        changed_prize.prize = result
+        changed_prize.save()
         super(ProductDiscount, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.product.title
-    
-
-
